b2a6468a962fe97ab4be9fab7df0f42d07a1a495
Daniel McNerthney <dan.mcnerthney@softllc.com>
2013-11-28 17:19:46 -0800

    reorder led

--- grade/assignment.js
+++ grade/assignment.js
@@ -1,8 +1,8 @@
 function AssignmentCtrl($scope) {
   $scope.assignments = [
-  {led: 0, due: 'Monday, Dec 2nd', subject: 'History', task: '11/26 margin notes', status: 'in-progress' },
-  {led: 1, due: 'Monday, Dec 2nd', subject: 'Writer Workshop', task: 'Dialog Entry', status: 'in-progress' },
-  {led: 2, due: 'Monday, Dec 2nd', subject: 'Writer Workshop', task: 'Found Poem', status: 'in-progress' },
-  {led: 3, due: 'Monday, Dec 2nd', subject: 'Spanish', task: '11/22 Friday Homework', status: 'in-progress' },
+  {led: 3, due: 'Monday, Dec 2nd', subject: 'History', task: '11/26 margin notes', status: 'in-progress' },
+  {led: 2, due: 'Monday, Dec 2nd', subject: 'Writer Workshop', task: 'Dialog Entry', status: 'in-progress' },
+  {led: 1, due: 'Monday, Dec 2nd', subject: 'Writer Workshop', task: 'Found Poem', status: 'in-progress' },
+  {led: 0, due: 'Monday, Dec 2nd', subject: 'Spanish', task: '11/22 Friday Homework', status: 'in-progress' },
   ];
 }
