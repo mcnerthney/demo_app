f067435e71c7781e5db6ca60f1dcb038072a58a2
dan mcnerthney <dan@softllc.com>
2013-12-09 16:56:59 -0800

    update

--- grade/assignment.js
+++ grade/assignment.js
@@ -1,12 +1,10 @@
 function AssignmentCtrl($scope) {
     $scope.assignments = [
-
-        {led: 10, due: 'Nov 29th', subject: 'Writer Workshop', task: 'Literary Postcards', status: 'missing' },
-        {led: 9, due: 'Mon, Dec 9th', subject: 'Science', task: 'definition homework', status: 'in-progress' },
-        {led: 8, due: 'not assigned', subject: 'Writer Workshop', task: 'pet peeves', status: 'unassigned' },
-
-        {led: 7, due: 'Mon, Dec 9th', subject: 'History', task: '5 question video', status: 'in-progress' },
-        {led: 6, due: 'Mon, Dec 9th', subject: 'Spanish', task: 'art poster', status: 'in-progress' },
+        {led: 10, due: 'Nov 29th', subject: 'Writer Workshop', task: 'Literary Postcards', status: 'completed' },
+        {led: 9, due: 'Tues, Dec 10th', subject: 'Science', task: 'worksheet homework', status: 'in-progress' },
+        {led: 8, due: 'Fri, Dec 13th', subject: 'Writer Workshop', task: 'pet peeves', status: 'in-progress' },
+        {led: 7, due: 'Mon, Dec 9th', subject: 'History', task: '5 question video', status: 'completed' },
+        {led: 6, due: 'Mon, Dec 9th', subject: 'Spanish', task: 'art poster', status: 'completed' },
 
         {led: 5, due: 'Thurs, Dec 5th', subject: 'English', task: 'essay ', status: 'completed' },
 
